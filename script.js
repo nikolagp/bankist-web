d1cc8a7adf9175150f456c20ed0dd09526e5e8a8
nikolagp <petrovskinikolag@gmail.com>
2022-01-19 12:02:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,8 +20,6 @@ const closeModal = function () {
 };
 
 btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
-// for (let i = 0; i < btnsOpenModal.length; i++)
-//   btnsOpenModal[i].addEventListener("click", openModal);
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
